3bbf2f964fcb0ce37752d61cfcbadcce4041e22f
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-01-09 14:18:20 +0000

    Deploying to gh-pages from @ vadimvatsenko/goit-react-hw-05-movies@f2c52de232bec9d908a56ba81a980d0d85c764ee 🚀

---
+++ static/js/922.80496479.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[922],{922:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});n(791);var i=n(184);function r(){return(0,i.jsxs)("main",{style:{textAlign:"center"},children:[(0,i.jsx)("b",{style:{fontSize:64},children:"404"}),(0,i.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=922.80496479.chunk.js.map
